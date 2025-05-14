f340f55bfd1ebac0f44cdd8cb5b0511a35258123
unknown <warren962244@gmail.com>
2025-05-14 10:53:05 +0800

    add name, title

--- src/Components/AboutMe.jsx
+++ src/Components/AboutMe.jsx
@@ -5,6 +5,7 @@ function AboutMe() {
         <section className = "about-me">
         <h2>About Me</h2>
         <div>
+            <h2 id="name">Hane96</h2>
             <p>Hi! 我是政大資訊科學系的畢業生</p>
             <p>目前主要學習前端相關的技術</p>
             <p>以前端工程師為目標，並持續探索新技術</p>
